3efbb681d3943273fe68e419cf3f5b55d9dd4825
kualta <contact@kualta.dev>
2026-01-11 11:13:53 -0600

    add i18n middleware for docs locale routing

---
+++ middleware.ts
@@ -0,0 +1,8 @@
+import { createI18nMiddleware } from "fumadocs-core/i18n/middleware";
+import { i18n } from "~/utils/i18n";
+
+export default createI18nMiddleware(i18n);
+
+export const config = {
+	matcher: ["/docs/:path*", "/:lang/docs/:path*"],
+};
