4073529bc2a0b72623387b30e414f892274af0b4
kualta <contact@kualta.dev>
2026-02-25 08:21:55 -0600

    remove fumadocs lastModified plugin for docker compatibility

--- source.config.ts
+++ source.config.ts
@@ -1,10 +1,7 @@
 import { defineDocs, defineConfig } from "fumadocs-mdx/config";
-import lastModified from "fumadocs-mdx/plugins/last-modified";
 
 export const docs = defineDocs({
   dir: "docs",
 });
 
-export default defineConfig({
-  plugins: [lastModified()],
-});
+export default defineConfig({});
